470a997d22080ef094cfed4c385116877f9465d0
Aaryan Chaturvedi <aaryan@Aaryans-Air.domain.name>
2020-07-12 23:53:25 +0530

    designing basic functionality

---
+++ jQuery/Todo.js
@@ -0,0 +1,18 @@
+$("ul").on("click" , "li" , function(){
+   $(this).toggleClass("completed");
+})
+//difference between using "click" and on("click") is that click works only on the single method while on(click) will works on multiple methods of same type
+$("ul").on("click" , "span" , function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text' ").keypress(function(event){
+    if(event.which === 13){ // 13 is the code for enter key
+        var todo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todo + "</li>"); //append is used to add any new line in the list using input
+    }
+}); 
