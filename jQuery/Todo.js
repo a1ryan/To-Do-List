a695032c665acdc977dd3bd9a5d78b8a35c0a7ef
Aaryan Chaturvedi <aaryan@Aaryans-Air.domain.name>
2020-07-13 15:50:37 +0530

    styling using css

--- jQuery/Todo.js
+++ jQuery/Todo.js
@@ -13,6 +13,9 @@ $("input[type='text' ").keypress(function(event){
     if(event.which === 13){ // 13 is the code for enter key
         var todo = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span> " + todo + "</li>"); //append is used to add any new line in the list using input
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todo + "</li>"); //append is used to add any new line in the list using input
     }
 }); 
+$(".fa-plus").click(function(){
+    $("input[type='text' ").fadeToggle();
+});
